4116e1b0ed16c48652f6a1c393091a0ab48b6c06
Spykillerx <mazenpppy@gmail.com>
2024-11-18 07:39:32 +0200

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=rJsAFIxL#mKlZZtVgwgCRwhWCrs5JP4_Ki6ePIiQ2nd3F-vWT6qE",
 };
